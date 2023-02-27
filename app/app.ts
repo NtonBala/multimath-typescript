e389030ea100e47e8f1da6dd4a0506e0dd841cca
Anton Balashov <balashov.anton.1983@gmail.com>
2023-02-27 21:00:31 +0200

    Update project configuration

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+function startGame() {
+  // starting a new game
+  const messagesElement = document.getElementById("messages");
+
+  messagesElement.innerText = "Welcome to MultiMath! Starting new game...";
+}
+
+document.getElementById("startGame").addEventListener("click", startGame);
