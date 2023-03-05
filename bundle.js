c1052aea1145d0522f9a855745af407e18dd2261
Anton Balashov <balashov.anton.1983@gmail.com>
2023-03-05 17:59:07 +0200

    Updates

--- bundle.js
+++ bundle.js
@@ -1,16 +1,2 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
-/*!********************!*\
-  !*** ./app/app.ts ***!
-  \********************/
-
-function startGame() {
-    var messagesElement = document.getElementById("messages");
-    messagesElement.innerText = "Welcome to MultiMath! Starting new game...";
-}
-document.getElementById("startGame").addEventListener("click", startGame);
-
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFhO0FBRWIsU0FBUyxTQUFTO0lBRWhCLElBQU0sZUFBZSxHQUFHLFFBQVEsQ0FBQyxjQUFjLENBQUMsVUFBVSxDQUFDLENBQUM7SUFFNUQsZUFBZ0IsQ0FBQyxTQUFTLEdBQUcsNENBQTRDLENBQUM7QUFDNUUsQ0FBQztBQUVELFFBQVEsQ0FBQyxjQUFjLENBQUMsV0FBVyxDQUFFLENBQUMsZ0JBQWdCLENBQUMsT0FBTyxFQUFFLFNBQVMsQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbXVsdGltYXRoLXR5cGVzY3JpcHQvLi9hcHAvYXBwLnRzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xuXG5mdW5jdGlvbiBzdGFydEdhbWUoKSB7XG4gIC8vIHN0YXJ0aW5nIGEgbmV3IGdhbWVcbiAgY29uc3QgbWVzc2FnZXNFbGVtZW50ID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJtZXNzYWdlc1wiKTtcblxuICBtZXNzYWdlc0VsZW1lbnQhLmlubmVyVGV4dCA9IFwiV2VsY29tZSB0byBNdWx0aU1hdGghIFN0YXJ0aW5nIG5ldyBnYW1lLi4uXCI7XG59XG5cbmRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwic3RhcnRHYW1lXCIpIS5hZGRFdmVudExpc3RlbmVyKFwiY2xpY2tcIiwgc3RhcnRHYW1lKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+(()=>{"use strict";document.getElementById("startGame").addEventListener("click",(function(){document.getElementById("messages").innerText="Welcome to MultiMath! Starting new game..."}))})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiJtQkFTQUEsU0FBU0MsZUFBZSxhQUFjQyxpQkFBaUIsU0FQdkQsV0FFMEJGLFNBQVNDLGVBQWUsWUFFL0JFLFVBQVksNENBQy9CLEciLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9tdWx0aW1hdGgtdHlwZXNjcmlwdC8uL2FwcC9hcHAudHMiXSwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG5cbmZ1bmN0aW9uIHN0YXJ0R2FtZSgpIHtcbiAgLy8gc3RhcnRpbmcgYSBuZXcgZ2FtZVxuICBjb25zdCBtZXNzYWdlc0VsZW1lbnQgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcIm1lc3NhZ2VzXCIpO1xuXG4gIG1lc3NhZ2VzRWxlbWVudCEuaW5uZXJUZXh0ID0gXCJXZWxjb21lIHRvIE11bHRpTWF0aCEgU3RhcnRpbmcgbmV3IGdhbWUuLi5cIjtcbn1cblxuZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJzdGFydEdhbWVcIikhLmFkZEV2ZW50TGlzdGVuZXIoXCJjbGlja1wiLCBzdGFydEdhbWUpO1xuIl0sIm5hbWVzIjpbImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJhZGRFdmVudExpc3RlbmVyIiwiaW5uZXJUZXh0Il0sInNvdXJjZVJvb3QiOiIifQ==
