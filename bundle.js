ab0b51dd22eacf7c9fd10dd870dcc64e63080159
Anton Balashov <balashov.anton.1983@gmail.com>
2023-03-05 17:33:20 +0200

    Updates

---
+++ bundle.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./app/app.ts ***!
+  \********************/
+
+function startGame() {
+    var messagesElement = document.getElementById("messages");
+    messagesElement.innerText = "Welcome to MultiMath! Starting new game...";
+}
+document.getElementById("startGame").addEventListener("click", startGame);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFhO0FBRWIsU0FBUyxTQUFTO0lBRWhCLElBQU0sZUFBZSxHQUFHLFFBQVEsQ0FBQyxjQUFjLENBQUMsVUFBVSxDQUFDLENBQUM7SUFFNUQsZUFBZ0IsQ0FBQyxTQUFTLEdBQUcsNENBQTRDLENBQUM7QUFDNUUsQ0FBQztBQUVELFFBQVEsQ0FBQyxjQUFjLENBQUMsV0FBVyxDQUFFLENBQUMsZ0JBQWdCLENBQUMsT0FBTyxFQUFFLFNBQVMsQ0FBQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbXVsdGltYXRoLXR5cGVzY3JpcHQvLi9hcHAvYXBwLnRzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xuXG5mdW5jdGlvbiBzdGFydEdhbWUoKSB7XG4gIC8vIHN0YXJ0aW5nIGEgbmV3IGdhbWVcbiAgY29uc3QgbWVzc2FnZXNFbGVtZW50ID0gZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJtZXNzYWdlc1wiKTtcblxuICBtZXNzYWdlc0VsZW1lbnQhLmlubmVyVGV4dCA9IFwiV2VsY29tZSB0byBNdWx0aU1hdGghIFN0YXJ0aW5nIG5ldyBnYW1lLi4uXCI7XG59XG5cbmRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwic3RhcnRHYW1lXCIpIS5hZGRFdmVudExpc3RlbmVyKFwiY2xpY2tcIiwgc3RhcnRHYW1lKTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
