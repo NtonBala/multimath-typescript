8fb5dcc12fd041cc2a4601fa6500f8e6c5fcfa6c
Anton Balashov <balashov.anton.1983@gmail.com>
2023-03-11 17:31:30 +0200

    Enable --strictNullChecks compiler option

--- app/app.ts
+++ app/app.ts
@@ -3,8 +3,14 @@
 function startGame() {
   // starting a new game
   const messagesElement = document.getElementById("messages");
+  let playerName: string = "Audrey";
 
+  logPlayer(playerName);
   messagesElement!.innerText = "Welcome to MultiMath! Starting new game...";
+}
+
+function logPlayer(name) {
+  console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
