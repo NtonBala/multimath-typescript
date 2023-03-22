4ab9d106eb5ea60b0a8cc8baf920c64523d6ef72
Anton Balashov <balashov.anton.1983@gmail.com>
2023-03-22 22:04:37 +0200

    Enable --noImplicitAny compiler option

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 function startGame() {
   // starting a new game
-  const messagesElement = document.getElementById("messages");
-  let playerName: string = "Audrey";
+  const messagesElement = document.getElementById('messages');
+  let playerName: string = 'Audrey';
 
   logPlayer(playerName);
-  messagesElement!.innerText = "Welcome to MultiMath! Starting new game...";
+  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
 
-function logPlayer(name) {
+function logPlayer(name: string): void {
   console.log(`New game starting for player: ${name}`);
 }
 
-document.getElementById("startGame")!.addEventListener("click", startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
