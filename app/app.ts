46aea145639f731d974c2342a436cd000209838c
Anton Balashov <balashov.anton.1983@gmail.com>
2023-03-23 21:58:34 +0200

    Add posting scores functionality

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,33 @@
 'use strict';
 
+const DEFAULT_PLAYERNAME = 'MultiMath Player';
+
 function startGame() {
   // starting a new game
-  const messagesElement = document.getElementById('messages');
-  let playerName: string = 'Audrey';
+  const playerName: string | undefined = getInputValue('playername');
 
   logPlayer(playerName);
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+  postScores(100, playerName);
 }
 
-function logPlayer(name: string): void {
+function logPlayer(name: string = DEFAULT_PLAYERNAME): void {
   console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+
+  if (inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScores(score: number, playerName: string = DEFAULT_PLAYERNAME): void {
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+
+  scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
