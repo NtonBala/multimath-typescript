2b0eec033a2d31d1eff9c7ba814f4205fae4fa95
Anton Balashov <balashov.anton.1983@gmail.com>
2023-04-27 14:54:52 +0300

    Configure webpack to bundle modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/app.js',
   mode: 'development',
-  // entry: "./app/app.ts",
-  // devtool: 'inline-source-map',
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.tsx?$/,
-  //       use: 'ts-loader',
-  //       exclude: /node_modules/,
-  //     },
-  //   ],
-  // },
-  // resolve: {
-  //   extensions: ['.tsx', '.ts', '.js'],
-  // },
-  // output: {
-  //   filename: 'bundle.js',
-  // },
+  entry: './app/app.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+  },
   devServer: {
     static: path.join(__dirname, './'),
     port: 8080,
